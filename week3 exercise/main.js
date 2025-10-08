d62993d0a41c608cfd71d9e85f14243baf594c45
Nejazi Zimberi <nz31062@seeu.edu.mk>
2025-10-08 10:49:20 +0200

    .

--- week3 exercise/main.js
+++ week3 exercise/main.js
@@ -5,7 +5,11 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x202020);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(3, 3, 6);
+camera.position.x = 0;
+camera.position.y = 2;
+camera.position.z = 5;
+camera.lookAt(0, 0, 0);
+
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
